30766bf3cac1ea9e34b26c42803bcf1e561c9d86
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2022-12-21 23:10:17 +0530

    #include <stdio.h>
    int main()
    {
        int i, num;
    
        
        scanf("%d", &num);
    
        for(i=1; i<=12; i++)
        {
            printf("%d x %d = %d
    ", num, i, (num*i));
        }
    
        return 0;
    }

---
+++ Multiplication_Table_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, num;
+
+    
+    scanf("%d", &num);
+
+    for(i=1; i<=12; i++)
+    {
+        printf("%d x %d = %d
+", num, i, (num*i));
+    }
+
+    return 0;
+}
