6fba8065a96d6ee2f48fc9274ad083e467316471
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2023-01-28 09:45:35 +0530

    #include<stdio.h> void pattern2(int n) { 	int i,j; 	for(i=1;i<n+1;i++) 	{ 		for(j=1;j<i+1;j++) 		{ 			printf("%d",j); 		} 		printf(" "); 	} } int main() {
    int n;
    scanf("%d",&n);
    pattern2(n);
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
-void pattern(int n)
+void pattern2(int n)
 {
-    int i,j;
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n-i+1;j++)
-        printf("%d",j);
-        printf("
+	int i,j;
+	for(i=1;i<n+1;i++)
+	{
+		for(j=1;j<i+1;j++)
+		{
+			printf("%d",j);
+		}
+		printf("
 ");
-    }
+	}
 }
 int main()
 {
-    int n; 
-    scanf("%d",&n);
-    pattern(n);
+
+int n;
+scanf("%d",&n);
+pattern2(n);
 }
