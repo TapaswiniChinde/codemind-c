f384e04f9d28877cb76e6035c049db9db91dc6ae
TapaswiniChinde <119723461+TapaswiniChinde@users.noreply.github.com>
2023-05-22 18:59:20 +0530

    #include<stdio.h> int main() {     int n,d,a[100],i,j;     scanf("%d",&n);     i=0;     while(n!=0){         d=n%10;         n=n/10;         a[i]=d;         i++;     }     for(j=i-1;j>=0;j--){         if(a[j]==6){             a[j]=9;             break;         }     }     for(j=i-1;j>=0;j--){         printf("%d",a[j]);     } }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,31 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,f,s,t,i;
+    int n,d,a[100],i,j;
     scanf("%d",&n);
-    i=n%10;
-    n=n/10;
-    t=n%10;
-    n=n/10;
-    s=n%10;
-    n=n/10;
-    f=n%10;
-    n=n/10;
-    if(f==6)
-    {
-        f=9;
+    i=0;
+    while(n!=0){
+        d=n%10;
+        n=n/10;
+        a[i]=d;
+        i++;
     }
-    else if(s==6)
-    {
-        s=9;
+    for(j=i-1;j>=0;j--){
+        if(a[j]==6){
+            a[j]=9;
+            break;
+        }
     }
-    else if(t==6)
-    {
-        t=9;
+    for(j=i-1;j>=0;j--){
+        printf("%d",a[j]);
     }
-    else if(i==6)
-    {
-        i=9;
-    }
-    printf("%d%d%d%d",f,s,t,i);
 }
